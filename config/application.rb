5fbcb0865d391bf2a6173c980b9a2dcbf8481e6d
Ryan Kliemann <Ryan_blake@icloud.com>
2015-12-20 15:47:49 -0800

    s

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,8 @@ module Blocipedia
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.assets.initialize_on_precompile = false
+    ActionMailer::Base.delivery_method = :smtp
+
 
   end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,17 @@
-# Load the Rails application.
+# Load the rails application
 require File.expand_path('../application', __FILE__)
 
-# Initialize the Rails application.
-Rails.application.initialize!
+# Initialize the rails application
+Blocipedia::Application.initialize!
+
+# Configuration for using SendGrid on Heroku
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  :user_name => "app45264104@heroku.com",
+  :password => "s9ouhqjp1934",
+  :domain => "localhost",
+  :address => "smtp.sendgrid.net",
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
