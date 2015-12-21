7c25e3059415b7313b81e3fcd587ea6ba8bbff29
Ryan Kliemann <Ryan_blake@icloud.com>
2015-12-20 19:54:10 -0800

    m

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,19 @@
+class UserController < ApplicationController
+  def show
+    @user = current_user
+
+    if params[:id]
+      @user = User.find(params[:id])
+    end
+
+  end
+  
+  # private
+  #
+  # def user_not_authorized
+  #   flash[:alert] = "You are not cool enough to do this - go back from whence you came."
+  #   redirect_to(root_path)
+  # end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  
+
   authenticated :user do
-  root to: 'users#show', as: :authenticated_root
+  root to: 'user#show', as: :authenticated_root
 end
 # root to: "users#show", as: :authenticated_root
 
